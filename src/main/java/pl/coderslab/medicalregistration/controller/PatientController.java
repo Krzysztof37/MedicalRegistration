6c0c6e6824c4b8ec3b4021c30af6f65e0fb08434
Krzysztof Jez <krzysztofjez37@tlen.pl>
2021-10-07 22:08:03 +0200

    wyk

--- src/main/java/pl/coderslab/medicalregistration/controller/PatientController.java
+++ src/main/java/pl/coderslab/medicalregistration/controller/PatientController.java
@@ -1,21 +1,30 @@
 package pl.coderslab.medicalregistration.controller;
 
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.medicalregistration.entity.Patient;
 import pl.coderslab.medicalregistration.entity.Procedure;
 import pl.coderslab.medicalregistration.utils.PatientRepository;
 import pl.coderslab.medicalregistration.utils.ProcedureRepository;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
+@RestController
 public class PatientController {
 
     private final PatientRepository patientRepository;
@@ -27,12 +36,15 @@ public class PatientController {
         this.procedureRepository = procedureRepository;
     }
 
-    @GetMapping("/patients/getall")
-    public String allPatients(Model model){
+    @GetMapping(value = "/patients/getall")
+    public String allPatients(Model model, HttpServletResponse resp){
+        resp.setHeader("Access-Control-Allow-Origin", "*");
         List<Patient> patientsList = patientRepository.findAll();
         model.addAttribute("patientsList", patientsList);
+        Gson gson = new Gson();
+        String newList = gson.toJson(patientsList);
 
-        return "allpatients";
+        return newList;
     }
 
     @GetMapping("/patients/add")
