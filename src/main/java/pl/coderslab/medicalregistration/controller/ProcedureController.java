3793e057bc662f44907275901bdda54c3a9e7081
Krzysztof Jez <krzysztofjez37@tlen.pl>
2021-10-27 20:29:44 +0200

    wyk

--- src/main/java/pl/coderslab/medicalregistration/controller/ProcedureController.java
+++ src/main/java/pl/coderslab/medicalregistration/controller/ProcedureController.java
@@ -3,11 +3,7 @@ package pl.coderslab.medicalregistration.controller;
 
 import com.google.gson.Gson;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +13,6 @@ import pl.coderslab.medicalregistration.utils.ProcedureRepository;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class ProcedureController {
@@ -39,6 +34,10 @@ public class ProcedureController {
     }
 
 
+    @GetMapping("/procedure/add")
+    public List<String> addstations(){
+        return List.of("Metoda get");
+    }
 
     @PostMapping("/procedure/add")
     public Object addstationsPost(@Valid Procedure procedure, BindingResult result, HttpServletResponse resp) {
