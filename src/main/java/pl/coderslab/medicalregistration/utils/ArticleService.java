4cdeb39683e41053bbeabe71e5f68d9677c2cdce
Krzysztof Jez <krzysztofjez37@tlen.pl>
2021-10-29 22:30:01 +0200

    wyk

--- src/main/java/pl/coderslab/medicalregistration/utils/ArticleService.java
+++ src/main/java/pl/coderslab/medicalregistration/utils/ArticleService.java
@@ -15,7 +15,7 @@ public class ArticleService {
 
     public Elements getArticleElements(String keyWord) throws IOException {
         String formattedKeyWord = keyWord.replaceAll(" ", "+");
-        Document doc = Jsoup.connect("https://scholar.google.com/scholar?as_ylo=2017&q=" + formattedKeyWord + "&hl=pl&num=60&as_sdt=0,5&as_vis=1")
+        Document doc = Jsoup.connect("http://scholar.google.com/scholar?as_ylo=2017&q=" + formattedKeyWord + "&hl=pl&num=60&as_sdt=0,5&as_vis=1")
                 .userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36").get();
 
 
